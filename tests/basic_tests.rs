d9f91274ca433877aa9a0ade67aea4c38ee28e9e
Masaki Hara <ackie.h.gmai@gmail.com>
2022-12-23 16:22:04 +0900

    Add tests for Eq for Point

--- tests/basic_tests.rs
+++ tests/basic_tests.rs
@@ -22,3 +22,18 @@ fn test_point_to_string() {
     let point = Point::new(1, 3);
     assert_eq!(point.to_string(), "(1, 3)");
 }
+
+#[test]
+fn test_point_eq() {
+    assert_eq!(Point::new(1, 3), Point::new(1, 3));
+}
+
+#[test]
+fn test_point_ne_y() {
+    assert_ne!(Point::new(1, 3), Point::new(1, 4));
+}
+
+#[test]
+fn test_point_ne_x() {
+    assert_ne!(Point::new(1, 3), Point::new(2, 3));
+}
